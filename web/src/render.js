727e32a018290c50a8f271e51b7b4299324869e2
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2019-09-22 13:13:46 +0200

    error handling

--- web/src/render.js
+++ web/src/render.js
@@ -13,7 +13,7 @@ module.exports =  express.Router()
 
         res.render(`pages${url}`, options);
     })
-    .use((err, req, res) => {
+    .use((err, req, res, next) => {
         const options = renderOptions(req);
 
         res.status(404).render('pages/index', options);
